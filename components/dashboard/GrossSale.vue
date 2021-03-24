613543a314dc63192000de8f6cb03a256bc7136d
Sirawit Pratoomsuwan <s36526@bj.ac.th>
2021-03-25 03:55:43 +0700

    add icon to dashboard.vue

--- components/dashboard/GrossSale.vue
+++ components/dashboard/GrossSale.vue
@@ -1,28 +1,43 @@
 <template>
   <v-card elevation="0" class="cshadow text-center">
-    <v-card-title>Today</v-card-title>
+    <v-card-title>✨ Today</v-card-title>
     <h3 class="text-center pt-4">Totals</h3>
     <h1 class="text-center py-0 pt-3 font-weight-bold display-2">
       $ 254,360,451
     </h1>
-    <h2 class="text-center pb-8 grey--text heading-5">Gross Sales</h2>
+    <h2 class="text-center pb-8 grey--text heading-5">
+      <v-icon>mdi-cube</v-icon> Gross Sales
+    </h2>
     <v-divider></v-divider>
     <v-row>
       <v-col md="4">
         <h3 class="text-center pt-4">Totals</h3>
         <h1 class="text-center py-0 pt-3 font-weight-bold display-2">18</h1>
-        <h2 class="text-center pb-8 grey--text heading-5">Products Sale</h2>
+        <h2 class="text-center pb-8 grey--text heading-5">
+          <v-icon>mdi-cube-send</v-icon> Products Sale
+        </h2>
       </v-col>
       <v-col md="4">
         <h3 class="text-center pt-4">Totals</h3>
         <h1 class="text-center py-0 pt-3 font-weight-bold display-2">365</h1>
-        <h2 class="text-center pb-8 grey--text heading-5">Products in stock</h2>
+        <h2 class="text-center pb-8 grey--text heading-5">
+          <v-icon>mdi-cube-scan</v-icon> Product in stock
+        </h2>
       </v-col>
       <v-col md="4">
         <h3 class="text-center pt-4">Totals</h3>
         <h1 class="text-center py-0 pt-3 font-weight-bold display-2">26</h1>
-        <h2 class="text-center pb-8 grey--text heading-5">Customers</h2>
+        <h2 class="text-center pb-8 grey--text heading-5">
+          <v-icon>mdi-clipboard-account-outline</v-icon> Customers
+        </h2>
       </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col md="4" sm="12" class="px-8"
+        ><v-btn block tile color="primary"
+          ><v-icon left>mdi-printer</v-icon>Print Report</v-btn
+        ></v-col
+      >
     </v-row>
   </v-card>
 </template>
